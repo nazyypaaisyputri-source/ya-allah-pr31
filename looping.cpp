98a73c20c5fd0e4c80147ac90b7079ba98618d2d
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-31 20:45:10 +0700

    menambahkan implementasi perulangan pada program

--- looping.cpp
+++ looping.cpp
@@ -3,6 +3,14 @@
 #include <string>
 using namespace std;
 
-int main(){
+int main() {
+    int x;
+
+    cout<<"PERULANGAN FOR"<<endl;
+    for(int i=0; i<=4; i++){
+        cout<<"Teknologi Informasi UMY"<<endl;
+    }
+    cout<<endl;
+    
     return 0;
 }
