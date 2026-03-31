0e66ae470592c94d0dfeac303b91cac904067efd
nazyypaaisyputri-source <nazyypaaisyputri@gmail.com>
2026-03-31 21:08:55 +0700

    menambahkan logika OR dengan operator NOT

--- looping.cpp
+++ looping.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main() {
     int x;
 
-    // --- BAGIAN PERULANGAN (Commit 2, 3, 4) ---
     cout << "PERULANGAN FOR" << endl;
     for(int i = 0; i <= 4; i++) {
         cout << "Teknologi Informasi UMY" << endl;
@@ -30,7 +29,6 @@ int main() {
     } while(x >= 5);
     cout << "Bilangan acak do-while yang terakhir = " << x << endl << endl;
 
-    // --- BAGIAN LOGIKA NOT (Commit 5) ---
     float nilB, nilM, rerata;
     string status;
     cout << "--- Logika NOT ---" << endl;
@@ -40,8 +38,23 @@ int main() {
 
     if (!(rerata < 60)) status = "Lulus";
     else status = "Tidak lulus";
-    
     cout << "Status kelulusan = " << status << " ,dengan nilai rata rata = " << rerata << endl << endl;
+
+    cout << "--- Logika OR ---" << endl;
+    if (!(rerata >= 60) || (nilM >= 70)) {
+        status = "Lulus";
+    } else {
+        status = "Tidak lulus";
+    }
+    cout << "Status kelulusan = " << status << " ,dengan nilai rata rata = " << rerata << endl << endl;
+
+    cout << "--- Logika AND ---" << endl;
+    if ((rerata >= 60) && (nilM >= 70)) {
+        status = "Lulus";
+    } else {
+        status = "Tidak lulus";
+    }
+    cout << "Status kelulusan = " << status << " ,dengan nilai rata rata = " << rerata << endl;
 
     return 0;
 }
